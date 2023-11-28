2da3f59350a26ed9bf659b3fb9e3d994141839cc
Diego Lima <diego.ktw@gmail.com>
2023-11-28 14:38:03 -0300

    Atualizando o main do dev

--- infra/main.tf
+++ infra/main.tf
@@ -22,7 +22,7 @@ resource "aws_launch_template" "maquina" {
     Name = "Machine-TAP"
   }
   security_group_names = [ var.grupodeseguranca ]
-  user_data = filebase64("ansible.sh")
+  user_data = var.producao ? filebase64("ansible.sh") : ""
 }
 
 resource "aws_key_pair" "chaveSSH" {
@@ -35,11 +35,11 @@ resource "aws_autoscaling_group" "grupo" {
   name = var.nomeGrupo
   max_size = var.maximo
   min_size = var.minimo
+  target_group_arns = var.producao ? [ aws_lb_target_group.alvoLoadBalancer[0].arn ] : []
   launch_template {
     id = aws_launch_template.maquina.id
     version = "$Latest"
-  }
-  target_group_arns = [ aws_lb_target_group.alvoLoadBalancer.arn ]
+  }  
 }
 
 resource "aws_default_subnet" "subnet_1" {
@@ -53,6 +53,7 @@ resource "aws_default_subnet" "subnet_2" {
 resource "aws_lb" "loadBalancer" {
   internal = false
   subnets = [ aws_default_subnet.subnet_1.id, aws_default_subnet.subnet_2.id ]
+  count = var.producao ? 1 : 0
 }
 
 resource "aws_default_vpc" "default" {
@@ -63,16 +64,18 @@ resource "aws_lb_target_group" "alvoLoadBalancer" {
   port = "8000"
   protocol = "HTTP"
   vpc_id = aws_default_vpc.default.id
+  count = var.producao ? 1 : 0
 }
 
 resource "aws_lb_listener" "entradaLoadBalancer" {
-  load_balancer_arn = aws_lb.loadBalancer.arn
+  load_balancer_arn = aws_lb.loadBalancer[0].arn
   port = "8000"
   protocol = "HTTP"
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.alvoLoadBalancer.arn
+    target_group_arn = [ aws_lb_target_group.alvoLoadBalancer[0].arn ]
   }
+  count = var.producao ? 1 : 0
 }
 
 resource "aws_autoscaling_policy" "escala-producao" {
@@ -85,4 +88,5 @@ resource "aws_autoscaling_policy" "escala-producao" {
     }
     target_value = 50.0
   }
+  count = var.producao ? 1 :0
 }
